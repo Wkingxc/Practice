669d7eff1a43ba52e0ba33f931935cabf2cdadfc
wpyxc <73375688+wpyxc@users.noreply.github.com>
2021-04-09 20:23:10 +0800

    第二版本，增加了不确定度B的输入，以及丰富了过程量的打印输出

--- src/practice/Solve.java
+++ src/practice/Solve.java
@@ -1,8 +1,9 @@
 package practice;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
 import javax.swing.*;
+import static java.lang.Math.*;
 
 public class Solve extends JFrame implements ActionListener {
     double[] t = new double[]{0.0, 0.0, 1.84, 1.32, 1.2, 1.14, 1.11, 1.09, 1.08, 1.07, 1.06, 1.05, 1.03};
@@ -12,12 +13,12 @@ public class Solve extends JFrame implements ActionListener {
     int ycount;
     JTextField tfx = new JTextField();
     JTextField tfy = new JTextField();
-    JTextField tf3 = new JTextField();
+    JTextArea tf3 = new JTextArea();
     JLabel label3 = new JLabel("cj.Wang");
     JLabel label1 = new JLabel("主要输入框(x)");
     JLabel label2 = new JLabel("可选输入框(y)");
     JButton button0 = new JButton("录入数据");
-    JButton button1 = new JButton("不确定度A");
+    JButton button1 = new JButton("不确定度");
     JButton button2 = new JButton("检测1次坏值");
     JButton button3 = new JButton("线性相关系数");
     JButton button4 = new JButton("线性回归方程");
@@ -32,43 +33,44 @@ public class Solve extends JFrame implements ActionListener {
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        this.label1.setBounds(30, 0, 300, 30);
-        this.label2.setBounds(30, 90, 300, 30);
-        this.label3.setBounds(430, 420, 120, 60);
-        this.button0.setBounds(30, 410, 100, 50);
-        this.button1.setBounds(350, 30, 120, 30);
-        this.button2.setBounds(350, 80, 120, 30);
-        this.button3.setBounds(350, 130, 120, 30);
-        this.button4.setBounds(350, 180, 120, 30);
-        this.button5.setBounds(200, 410, 100, 50);
-        this.button0.addActionListener(this);
-        this.button1.addActionListener(this);
-        this.button2.addActionListener(this);
-        this.button3.addActionListener(this);
-        this.button4.addActionListener(this);
-        this.button5.addActionListener(this);
-        this.tfx.setBounds(30, 30, 300, 50);
-        this.tfy.setBounds(30, 120, 300, 50);
-        this.tf3.setBounds(30, 200, 300, 200);
-        this.tf3.setEditable(false);
-        frame.getContentPane().add(this.button0);
-        frame.getContentPane().add(this.button1);
-        frame.getContentPane().add(this.button2);
-        frame.getContentPane().add(this.button3);
-        frame.getContentPane().add(this.button4);
-        frame.getContentPane().add(this.button5);
-        frame.getContentPane().add(this.label1);
-        frame.getContentPane().add(this.label2);
-        frame.getContentPane().add(this.label3);
-        frame.getContentPane().add(this.tf3);
-        frame.getContentPane().add(this.tfx);
-        frame.getContentPane().add(this.tfy);
+        label1.setBounds(30, 0, 300, 30);
+        label2.setBounds(30, 90, 300, 30);
+        label3.setBounds(430, 420, 120, 60);
+        button0.setBounds(30, 410, 100, 50);
+        button1.setBounds(350, 30, 120, 30);
+        button2.setBounds(350, 80, 120, 30);
+        button3.setBounds(350, 130, 120, 30);
+        button4.setBounds(350, 180, 120, 30);
+        button5.setBounds(200, 410, 100, 50);
+        button0.addActionListener(this);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+        button4.addActionListener(this);
+        button5.addActionListener(this);
+        tfx.setBounds(30, 30, 300, 50);
+        tfy.setBounds(30, 120, 300, 50);
+        tf3.setBounds(30, 200, 300, 200);
+        tf3.setEditable(false);
+        tf3.setFont(new Font("Dialog",Font.BOLD,20));
+        frame.getContentPane().add(button0);
+        frame.getContentPane().add(button1);
+        frame.getContentPane().add(button2);
+        frame.getContentPane().add(button3);
+        frame.getContentPane().add(button4);
+        frame.getContentPane().add(button5);
+        frame.getContentPane().add(label1);
+        frame.getContentPane().add(label2);
+        frame.getContentPane().add(label3);
+        frame.getContentPane().add(tf3);
+        frame.getContentPane().add(tfx);
+        frame.getContentPane().add(tfy);
         frame.setVisible(true);
     }
 
     double solve_uncertainty(double[] data, int cnt) {
-        double s = this.solve_Bessle(data, cnt);
-        return this.t[cnt] * s / Math.sqrt(cnt);
+        double s = solve_Bessle(data, cnt);
+        return t[cnt] * s / sqrt(cnt);
     }
 
     void getXData(String s) {
@@ -77,11 +79,11 @@ public class Solve extends JFrame implements ActionListener {
 
         for(int i = 0; i < len; ++i) {
             if (s.charAt(i) == ' ') {
-                this.x[this.xcount++] = Double.parseDouble(temp.toString());
+                x[xcount++] = Double.parseDouble(temp.toString());
                 temp = new StringBuilder();
             } else if (i == len - 1) {
                 temp.append(s.charAt(i));
-                this.x[this.xcount++] = Double.parseDouble(temp.toString());
+                x[xcount++] = Double.parseDouble(temp.toString());
             } else {
                 temp.append(s.charAt(i));
             }
@@ -93,11 +95,11 @@ public class Solve extends JFrame implements ActionListener {
         int len = s.length();
         for(int i = 0; i < len; ++i) {
             if (s.charAt(i) == ' ') {
-                this.y[this.ycount++] = Double.parseDouble(temp.toString());
+                y[ycount++] = Double.parseDouble(temp.toString());
                 temp = new StringBuilder();
             } else if (i == len - 1) {
                 temp.append(s.charAt(i));
-                this.y[this.ycount++] = Double.parseDouble(temp.toString());
+                y[ycount++] = Double.parseDouble(temp.toString());
             } else {
                 temp.append(s.charAt(i));
             }
@@ -111,35 +113,34 @@ public class Solve extends JFrame implements ActionListener {
         return sum / (double)cnt;
     }
     double solve_Bessle(double[] data, int cnt) {
-        double avrg = this.average(data, cnt);
+        double avrg = average(data, cnt);
         double sum = 0.0D;
 
         for(int i = 0; i < cnt; ++i) {
-            sum += Math.pow(data[i] - avrg, 2.0D);
+            sum += pow(data[i] - avrg, 2.0D);
         }
 
-        return Math.sqrt(sum / (double)(cnt - 1));
+        return sqrt(sum / (double)(cnt - 1));
     }
-
     void find_Badvalue() {
         String badvalue = "坏值有：";
         boolean flag = false;
-        double average = this.average(this.x, this.xcount);
-        double standard = this.solve_Bessle(this.x, this.xcount) * 3.0D;
+        double average = average(x, xcount);
+        double standard = solve_Bessle(x, xcount) * 3.0D;
 
-        for(int i = 0; i < this.xcount; ++i) {
-            if (Math.abs(this.x[i] - average) >= standard) {
+        for(int i = 0; i < xcount; ++i) {
+            if (abs(x[i] - average) >= standard) {
                 flag = true;
-                badvalue = badvalue + this.x[i] + " ";
+                badvalue = badvalue + x[i] + " ";
             }
         }
         if (flag) {
-            this.tf3.setText("2sigma为--->"+standard+"\n"+badvalue);
+            tf3.setText("3sigma为--->"+standard+"\n平均值为-->"+average+"\n"+badvalue);
         } else {
-            this.tf3.setText("2sigma为--->"+standard+"\n无坏值");
+            tf3.setText("3sigma为--->"+standard+
+                    "\n平均值为--->"+average+"\n无坏值");
         }
     }
-
     double xfangba(double[] data, int cnt) {
         double sum = 0.0D;
 
@@ -161,20 +162,20 @@ public class Solve extends JFrame implements ActionListener {
     }
 
     double solve_r(double[] x, double[] y, int cnt) {
-        double x_avrg = this.average(x, cnt);
-        double y_avrg = this.average(y, cnt);
-        double xfangba = this.xfangba(x, cnt);
-        double yfangba = this.xfangba(y, cnt);
-        double shang = this.xyba(x, y, cnt) - x_avrg * y_avrg;
+        double x_avrg = average(x, cnt);
+        double y_avrg = average(y, cnt);
+        double xfangba = xfangba(x, cnt);
+        double yfangba = xfangba(y, cnt);
+        double shang = xyba(x, y, cnt) - x_avrg * y_avrg;
         double xia = (xfangba - x_avrg * x_avrg) * (yfangba - y_avrg * y_avrg);
-        return shang / Math.sqrt(xia);
+        return shang / sqrt(xia);
     }
 
     String solve_linefc(double[] x, double[] y, int cnt) {
-        double xba = this.average(x, cnt);
-        double yba = this.average(y, cnt);
-        double xyba = this.xyba(x, y, cnt);
-        double xfb = this.xfangba(x, cnt);
+        double xba = average(x, cnt);
+        double yba = average(y, cnt);
+        double xyba = xyba(x, y, cnt);
+        double xfb = xfangba(x, cnt);
         double b = (xyba - xba * yba) / (xfb - xba * xba);
         double a = yba - b * xba;
         return "y= " + b + "x +" + a;
@@ -184,39 +185,40 @@ public class Solve extends JFrame implements ActionListener {
         Solve solve = new Solve();
         solve.go();
     }
-
     public void actionPerformed(ActionEvent e) {
         String method = e.getActionCommand();
         double result;
         switch (method) {
             case "录入数据" -> {
-                this.getXData(this.tfx.getText());
-                this.getYData(this.tfy.getText());
-                this.tf3.setText("成功！");
+                getXData(tfx.getText());
+                getYData(tfy.getText());
+                tf3.setText("成功！");
             }
-            case "不确定度A" -> {
-                result = this.solve_uncertainty(this.x, this.xcount);
-                DecimalFormat myformat = new DecimalFormat("0.0000000");
-                String str = myformat.format(result);
-                this.tf3.setText(str);
+            case "不确定度" -> {
+                double uncertainty_A = solve_uncertainty(x, xcount);
+                String str_B = JOptionPane.showInputDialog(null,"请输入：\n","请输入B类不确定度",JOptionPane.PLAIN_MESSAGE);
+                double uncertainty_B = Double.parseDouble(str_B);
+                double uncertainty = sqrt(pow(uncertainty_A,2)+pow(uncertainty_B,2));
+                tf3.setText("不确定度A--->"+uncertainty_A+"\n不确定度B为--->"+uncertainty_B
+                +"\n不确定度为--->"+uncertainty);
             }
-            case "检测1次坏值" -> this.find_Badvalue();
+            case "检测1次坏值" -> find_Badvalue();
             case "线性相关系数" -> {
-                result = this.solve_r(this.x, this.y, this.xcount);
-                this.tf3.setText("r = " + result);
+                result = solve_r(x, y, xcount);
+                tf3.setText("r = " + result);
             }
             case "线性回归方程" -> {
-                String s = this.solve_linefc(this.x, this.y, this.xcount);
-                this.tf3.setText(s);
+                String s = solve_linefc(x, y, xcount);
+                tf3.setText(s);
             }
             case "清空数据" -> {
-                this.x = new double[20];
-                this.y = new double[20];
-                this.xcount = 0;
-                this.ycount = 0;
-                this.tfx.setText("");
-                this.tfy.setText("");
-                this.tf3.setText("");
+                x = new double[20];
+                y = new double[20];
+                xcount = 0;
+                ycount = 0;
+                tfx.setText("");
+                tfy.setText("");
+                tf3.setText("");
             }
         }
     }
